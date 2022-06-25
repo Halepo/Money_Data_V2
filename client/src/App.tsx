9378e937016f68e8eb2be0b168d01142995d2751
Zemichael Mehretu <zemichaelmd@gmail.com>
2022-06-25 22:28:01 +0300

    Started new react client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>MoneyData v2</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <main className="container">App Name</main>
     </div>
   );
 }
