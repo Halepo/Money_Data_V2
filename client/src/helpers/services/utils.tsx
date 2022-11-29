388327543e4d494f0473874e9ea9524de0121838
zemichaelmd <zemichaelmd@gmail.com>
2022-11-29 12:43:43 +0300

    removed util

--- client/src/helpers/services/utils.tsx
+++ client/src/helpers/services/utils.tsx
@@ -1,17 +1,5 @@
 import jwtDecode from 'jwt-decode';
-import { useEffect, useState } from 'react';
 import { IDecodedJWT } from '../interface/authTypes';
-
-export const useBeforeRender = (callback: Function, deps: any) => {
-  const [isRun, setIsRun] = useState(false);
-
-  if (!isRun) {
-    callback();
-    setIsRun(true);
-  }
-
-  useEffect(() => () => setIsRun(false), deps);
-};
 
 export const decodeJWT = (JWT: string): IDecodedJWT => {
   return jwtDecode(JWT);
