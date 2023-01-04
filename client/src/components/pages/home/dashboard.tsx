496849f7896d314af8d93cb0102dd0593918ea91
melkam-mekonnen <mmm2477@cumc.columbia.edu>
2023-01-04 16:53:33 +0300

    Added mock stuff

--- client/src/components/pages/home/dashboard.tsx
+++ client/src/components/pages/home/dashboard.tsx
@@ -121,11 +121,38 @@ export default function Dashboard(props: any) {
               </p>
             </div>
             <div className="card-body p-3">
-              <div className="chart">Do chart here</div>
+              <div className="chart">
+                <img
+                  src="https://www.amcharts.com/wp-content/uploads/2019/02/demo_13290_none-1-1024x690.png"
+                  width="700px"
+                  height="300px"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-lg-5">Do another chart here</div>
+        <div className="col-lg-5 mb-lg-0 mb-4">
+          <div className="card z-index-2 h-100">
+            <div className="card-header pb-0 pt-3 bg-transparent">
+              <h6 className="text-capitalize">Others</h6>
+              <p className="text-sm mb-0">
+                <i className="bi bi-arrow-up"></i>
+                <span className="font-weight-bold">4% more</span> " in 2021 "
+              </p>
+            </div>
+            <div className="card-body p-3">
+              <div className="chart">
+                <img
+                  src="https://d2mvzyuse3lwjc.cloudfront.net/doc/en/UserGuide/images/2D_B_and_W_Pie_Chart/2D_B_W_Pie_Chart_1.png?v=83139"
+                  width="450px"
+                  height="350px"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="row mt-4">
         <div className="col-lg-12 mb-lg-0 mb-4">
