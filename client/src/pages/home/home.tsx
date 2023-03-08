d272ee4759c7c46bd5804142feb5ffeec995d387
zemichaelmd <zemichaelmd@gmail.com>
2023-03-09 01:21:43 +0300

    Merged home and dashboard

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,12 +1,78 @@
 import './home.sass';
-import Dashboard from './dashboard';
-import Accounts from './account/accounts';
+import TransactionsDataTable from '../transactions/transactionsDataTable';
+import StatCard from './components/statCard';
+import ActionButtons from './components/actionButtons';
+import Modal from '../layout/modal/modal';
 
 export default function Home(props: any) {
   console.log('Home rendered!');
   return (
     <div className="home-wrapper">
-      <Dashboard />
+      <div className="container-fluid py-4">
+        <ActionButtons />
+        <div className="row">
+          <StatCard />
+          <StatCard />
+          <StatCard />
+          <StatCard />
+
+          <div className="row mt-4">
+            <TransactionsDataTable />
+            <div className="col-lg-5">
+              <div className="card">
+                <div className="card-header pb-0 p-3">
+                  <h6 className="mb-0">Categories</h6>
+                </div>
+                <div className="card-body p-3">Hey</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-4"></div>
+          <div className="col-lg-7 mb-lg-0 mb-4">
+            <div className="card z-index-2 h-100">
+              <div className="card-header pb-0 pt-3 bg-transparent">
+                <h6 className="text-capitalize">Overview</h6>
+                <p className="text-sm mb-0">
+                  <i className="bi bi-arrow-up"></i>
+                  <span className="font-weight-bold">4% more</span> " in 2021 "
+                </p>
+              </div>
+              <div className="card-body p-3">
+                <div className="chart">
+                  <img
+                    src="https://www.amcharts.com/wp-content/uploads/2019/02/demo_13290_none-1-1024x690.png"
+                    width="700px"
+                    height="300px"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 mb-lg-0 mb-4">
+            <div className="card z-index-2 h-100">
+              <div className="card-header pb-0 pt-3 bg-transparent">
+                <h6 className="text-capitalize">Others</h6>
+                <p className="text-sm mb-0">
+                  <i className="bi bi-arrow-up"></i>
+                  <span className="font-weight-bold">4% more</span> " in 2021 "
+                </p>
+              </div>
+              <div className="card-body p-3">
+                <div className="chart">
+                  <img
+                    src="https://d2mvzyuse3lwjc.cloudfront.net/doc/en/UserGuide/images/2D_B_and_W_Pie_Chart/2D_B_W_Pie_Chart_1.png?v=83139"
+                    width="450px"
+                    height="350px"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
