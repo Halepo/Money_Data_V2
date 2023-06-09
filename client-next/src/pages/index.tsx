c17cf0592706569715b606d932957312f4a48557
Halepo <haleposolutions.in>
2023-06-09 17:17:16 +0300

    Added dashboard with shadcn

--- client-next/src/pages/index.tsx
+++ client-next/src/pages/index.tsx
@@ -3,16 +3,13 @@ import * as React from 'react';
 // import './App.sass';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+import { Button } from '@/components/ui/button';
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Seo templateTitle='Home' />
-      {/* <Seo /> */}
+   <main>
 
-      <main>
-        <section className='bg-white'></section>
-      </main>
-    </Layout>
+   </main>
   );
 }
+
